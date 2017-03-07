db35b78b7422f52065a66db193418b1225469d14
zhouwei <170491295@qq.com>
2017-03-07 18:38:03 +0800

    集成logback

--- platform-server/src/main/java/com/zhouwei/platform/service/test/impl/TestServiceImpl.java
+++ platform-server/src/main/java/com/zhouwei/platform/service/test/impl/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.zhouwei.platform.service.test.impl;
 import com.zhouwei.platform.bean.test.Test;
 import com.zhouwei.platform.mapper.test.TestMapper;
 import com.zhouwei.platform.service.test.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestServiceImpl implements TestService {
 
+    private static Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
+
     @Autowired
     TestMapper testMapper;
 
     public Test selectById(String id) {
+
+        log.info("info:"+id);
+        log.debug("debug:"+id);
+        log.warn("warn:"+id);
+        log.error("error:"+id);
+
+
         Test test = testMapper.selectById(id);
         return test;
     }
